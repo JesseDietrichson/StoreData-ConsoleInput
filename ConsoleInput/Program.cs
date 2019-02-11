0b7c4b49f4fee1b428c9f8e76ab24fe12bf0acfb
Jesse Dietrichson <jesse.dietrichson@xamarin.com>
2019-02-10 20:50:29 -0500

    Add project files.

---
+++ ConsoleInput/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine($"Hello {name}");
+
+
+            Console.WriteLine("Enter a number");
+
+            int number = int.Parse(Console.ReadLine());
+
+            double decimalNumber = double.Parse(Console.ReadLine());
+
+        }
+    }
+}
